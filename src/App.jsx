b918cc1cca7bdde9fad4cb410285d35462b86dc3
Tyler Muir <tylermuir42@gmail.com>
2026-02-13 10:48:12 -0700

    Added banner ads

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import SearchSection from './components/SearchSection'
 import Features from './components/Features'
 import About from './components/About'
 import Footer from './components/Footer'
+import leftAd from './images/Gemini_Generated_Image_30hp6530hp6530hp.png'
+import rightAd from './images/Gemini_Generated_Image_9bnvjj9bnvjj9bnv.png'
 import './App.css'
 
 function App() {
@@ -47,17 +49,25 @@ function App() {
 
     return (
         <div className="App">
-            <Header />
-            <Hero />
-            <SearchSection
-                onSearch={handleSearch}
-                loading={loading}
-                error={error}
-                restaurants={restaurants}
-            />
-            <Features />
-            <About />
-            <Footer />
+            <div className="ad-banner ad-banner-left">
+                <img src={leftAd} alt="Advertisement" />
+            </div>
+            <div className="main-content">
+                <Header />
+                <Hero />
+                <SearchSection
+                    onSearch={handleSearch}
+                    loading={loading}
+                    error={error}
+                    restaurants={restaurants}
+                />
+                <Features />
+                <About />
+                <Footer />
+            </div>
+            <div className="ad-banner ad-banner-right">
+                <img src={rightAd} alt="Advertisement" />
+            </div>
         </div>
     )
 }
